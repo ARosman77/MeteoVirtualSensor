7580d1ef360fac3fb497cb87dd8b0f9c807b4356
ARosman77 <ales.rosman@gmail.com>
2018-03-02 13:33:48 +0100

    Playing with dom objects.

--- meteo.py
+++ meteo.py
@@ -7,18 +7,21 @@ import urllib.request
 from xml.dom import minidom
 
 def fetchData():
+    #meteoURL = "http://www.meteo.si/uploads/probase/www/observ/surface/text/sl/observation_si_latest.xml"
     meteoURL = "http://www.meteo.si/uploads/probase/www/observ/surface/text/sl/observationAms_si_latest.xml"
     dom = minidom.parse(urllib.request.urlopen(meteoURL))
-    shortTitles = dom.getElementsByTagName('domain_shortTitle')
-    tsUpdated = dom.getElementsByTagName('tsUpdated')[1].firstChild.data
-    temperature = dom.getElementsByTagName('t_degreesC')[1].firstChild.data
-    humidity = dom.getElementsByTagName('rh')[1].firstChild.data
+    metData = dom.getElementsByTagName('metData')
+    #shortTitles = dom.getElementsByTagName('domain_shortTitle')
+    #tsUpdated = dom.getElementsByTagName('tsUpdated')[1].firstChild.data
+    #temperature = dom.getElementsByTagName('t_degreesC')[1].firstChild.data
+    #humidity = dom.getElementsByTagName('rh')[1].firstChild.data
 
-    for title in shortTitles:
-        print(title.firstChild.data)
-    print(tsUpdated)
-    print(temperature)
-    print(humidity)
+    for data in metData:
+        print(data.getElementsByTagName('domain_shortTitle').item(0).firstChild.data)
+        #print(data.firstChild.data)
+    #print(tsUpdated)
+    #print(temperature)
+    #print(humidity)
 
 fetchData()
 
