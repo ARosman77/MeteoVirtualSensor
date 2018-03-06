7490674d959f7fa8c792e69857e219092d200757
Ales@Zver2017 <ales.rosman@gmail.com>
2018-03-06 14:20:52 +0100

    Class implementation of meto.py
    Initial commit of class implementation of meteo.py. Not tested yet.

--- meteo.py
+++ meteo.py
@@ -9,35 +9,61 @@ import urllib.request
 from xml.dom import minidom
 
 # Meteo.si URLs
-#meteoURL = "http://www.meteo.si/uploads/probase/www/observ/surface/text/sl/observation_si_latest.xml"
-meteoURL = "http://www.meteo.si/uploads/probase/www/observ/surface/text/sl/observationAms_si_latest.xml"
+#testURL = "http://www.meteo.si/uploads/probase/www/observ/surface/text/sl/observation_si_latest.xml"
+testURL = "http://www.meteo.si/uploads/probase/www/observ/surface/text/sl/observationAms_si_latest.xml"
 
-def fetchData(url):
-    dom = minidom.parse(urllib.request.urlopen(url))
-    return dom.getElementsByTagName('metData')
+class meteoData:
+    # place for static global variables
+    def __init__(self, meteoURL, tagName, tagData):
+        self.tagName = tagName
+        self.tagData = tagData
+        self.meteoURL = meteoURL
+        self.domDocument = None
+        self.metData = None
+        self.tagNameData = None
+        # fetch data at creation
+        self.refreshData()
+    
+    def fetchData(self):
+        self.domDocument = minidom.parse(urllib.request.urlopen(self.meteoURL))
+        self.metData = self.domDocument.getElementsByTagName('metData')
+        return self.metData
+    
+    def getTagNameData(self):
+        for data in self.metData:
+            if ( data.getElementsByTagName(self.tagName).item(0).firstChild.data == self.tagData ):
+                self.tagNameData = data
+                return self.tagNameData
+        self.tagNameData = None
+        return self.tagNameData
 
-def getRegionData(metData, tagName, tagData):
-    for data in metData:
-        if ( data.getElementsByTagName(tagName).item(0).firstChild.data == tagData ):
-            return data
-    return None
+    def getDataElement(self, tagName):
+        element = self.tagNameData.getElementsByTagName(tagName).item(0).firstChild
+        if (element!=None): return element.data
+        else: return None
+        
+    def refreshData(self):
+        self.fetchData()
+        self.getTagNameData()
+        
+    def getLongTitle(self):
+        return getDataElement('domain_longTitle')
+        
+    def getTemperature(self):
+        return getDataElement('t')
+        
+    def getHumidity(self):
+        return getDataElement('rh')
+        
+    def getWindSpeed(self):
+        return getDataElement('ff_val')
 
-def getRegionDataElement(regionData, tagName):
-    element = regionData.getElementsByTagName(tagName).item(0).firstChild
-    if (element!=None): return element.data
-    else: return None
-
-regionData = getRegionData(fetchData(meteoURL),
-                           'domain_shortTitle',
-                           'LJUBLJANA - BEŽIGRAD')
-if (regionData != None):
-    dataTitle = getRegionDataElement(regionData,'domain_longTitle')
-    dataTemp = getRegionDataElement(regionData,'t')
-    dataHum = getRegionDataElement(regionData,'rh')
-    dataWindSpeed = getRegionDataElement(regionData,'ff_val') 
-    if (dataWindSpeed == None) : dataWindSpeed=""
+dataLjubljana = meteoData(testURL,'domain_shortTitle','LJUBLJANA - BEŽIGRAD')
+dataTitle = dataLjubljana.getLongTitle()
+dataTemp = dataLjubljana.getTemperature()
+dataHum = dataLjubljana.getHumidity()
+dataWindSpeed = dataLjubljana.getWindSpeed()
+if (dataWindSpeed == None) : dataWindSpeed=""
     print(dataTitle + " : " + dataTemp + "°C / " + dataHum + "%  " + dataWindSpeed)
 else:
     print("Region data doesn't exist!")
-
-
